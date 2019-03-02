31396337a3579bf2df0131dec2539deb1c36dfa1
Wilde <wildmat@186590d2c0b7.ant.amazon.com>
2019-03-01 22:32:14 -0800

    adding packagehash to gitignore

--- cmd/conditionalInstall.js
+++ cmd/conditionalInstall.js
@@ -2,6 +2,8 @@
 // this is doing the check from root directory. says /root/package.json modified
 // because npm link is conditional, it wouldn't work to deploy this to a fresh machine. 
 //  becaused startupScript couldn't run conditional-install
+// make sure packagehash.txt is in gitignore, otherwise the pi will pull it and think
+// that the package.json changed, which will trigger npm install, which updates package-lock.json too
 
 let installChanged = require('install-changed')
 let shell = require('shelljs');
