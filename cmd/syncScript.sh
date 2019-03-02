bde9fcd1340c2871eb710dfd69fc67e40613a059
Wilde <wildmat@186590d2c0b7.ant.amazon.com>
2019-03-01 23:10:19 -0800

    some comments

--- cmd/syncScript.sh
+++ cmd/syncScript.sh
@@ -1,4 +1,11 @@
-# TODO: rename this to syncScript.sh
+# when this script is renamed, the KRS-updatePi document will fail, because syncScript will not have been
+# pulled in on the pi. But the rename happens here on the file, in the updatePi document, and in .bashrc on the pi
+# so you gotta manually pull on the pi after renaming this... or have the git pulling happen outside of this script.
+# like have the updatePi document do a git pull before running any scripts.
+
+# essentially any git pull stuff needs to happen outside of a named, source control file.
+# either set in the bashrc under an alias
+# or in a runcommand document... neither of which are really source controlled.
 
 # start the ssm-agent
 # start the iot listener
